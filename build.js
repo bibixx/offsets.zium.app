211bf80f84543af292a71005de1463fed2e3607f
Bartosz Legięć <bartosz@legiec.io>
2023-06-27 23:22:31 +0200

    Make the build script a node script

---
+++ build.js
@@ -0,0 +1,22 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+(async function () {
+  const dirents = await fs.readdir(__dirname, { withFileTypes: true });
+  const files = dirents
+    .filter(
+      (dirent) =>
+        dirent.isFile() &&
+        path.extname(dirent.name) === ".json" &&
+        dirent.name !== "package.json" &&
+        dirent.name !== "index.json"
+    )
+    .map((dirent) =>
+      dirent.name.substring(
+        0,
+        dirent.name.length - path.extname(dirent.name).length
+      )
+    );
+
+  await fs.writeFile('index.json', JSON.stringify(files));
+})();
